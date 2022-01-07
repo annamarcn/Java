9fcb2017b8e9c8ce799f5ff21f7b1337ef201b0f
Anna Marcinkowska <amarcinkowska@spotify.com>
2022-01-07 16:02:58 +0100

    ArrayPractice progress

--- src/main/java/com/github/knifeofdreams/codekata/ArrayPractice.java
+++ src/main/java/com/github/knifeofdreams/codekata/ArrayPractice.java
@@ -3,50 +3,132 @@ package com.github.knifeofdreams.codekata;
 public class ArrayPractice {
 
   //  Add an item to the array and return the array.
-  public int[] add(int[] array, int item) {
-    return new int[0];
+  public int[] add(int[] array, int item)
+  {
+    int[] temp = new int[array.length + 1];
+
+    for(int i = 0; i < array.length; i++){
+      temp[i] = array[i];
+    }
+    temp[array.length] = item;
+
+    return temp;
   }
 
   //  Return the item at the given index. Experiment with what happens if you ask for an invalid index (e.g. negative index or an index >= the length of the array)!
   public int get(int[] array, int index) {
-    return 0;
+    return array[index];
   }
 
   //  Remove the item by a given index and then return the item.
   //  Experiment with what happens if you try to remove an item at an invalid index (e.g. negative index or an index >= the length of the array)!
-  public int remove(int[] array, int index) {
-    return 0;
+  public int[] remove(int[] array, int index)
+  {
+    int[] newArray = new int[array.length-1];
+
+    for (int i = index; i < array.length - 1; i++) {
+      array[i] = array[i + 1];
+    }
+    for(int i = 0; i < newArray.length; i++)
+    {
+      newArray[i] = array[i];
+    }
+
+    return newArray;
   }
 
   //  Return true if the array contains the item and false is it doesn't.
   public boolean contains(int[] array, Integer item) {
+    for(int i = 0; i < array.length; i++)
+    {
+      if(array[i] == item){
+        return true;
+      }
+    }
     return false;
   }
 
   //  Return the index of the given item or -1 if it's not present in the array.
   public int indexOf(int[] array, Integer item) {
-    return 0;
+    for(int i = 0; i < array.length; i++)
+    {
+      if(array[i] == item)
+      {
+        return i;
+      }
+    }
+    return -1;
   }
 
   //  Return the size of the array.
-  public int size(int[] array) {
-    return 0;
+  public int size(int[] array)
+  {
+    int count = 0;
+
+    for(int i = 0; i < array.length; i++)
+    {
+      count++;
+    }
+    return count;
   }
 
   //  Double the value of each element in the array and return the result.
-  public int[] doubleValues(int[] array) {
-    return new int[0];
+  public int[] doubleValues(int[] array)
+  {
+    for(int i = 0; i < array.length; i++)
+    {
+      array[i] *= 2;
+    }
+    return array;
   }
 
   //  Select even elements from the array and return the result.
   public int[] evenNumbers(int[] array) {
-    return new int[0];
+
+    int count = 0;
+
+    for(int i = 0; i < array.length; i++)
+    {
+      if(array[i]%2 == 0)
+      {
+        count++;
+      }
+    }
+
+    int[] newArray = new int[count];
+
+    for(int i = 0; i < array.length; i++)
+    {
+      if(array[i]%2==0)
+      {
+        //newArray.add(array[i],array); //ASK JUDIT: why doesn't this work?
+      }
+    }
+
+    return newArray;
   }
 
-  // Remove duplicate items from the array.
+  // Remove duplicate items from the array. Return the array. (Only works if sorted array. If not, I don't know how to solve it)
   public int[] removeDuplicates(int[] array) {
-    return new int[0];
+
+    int[] newArray = new int[array.length];
+
+    int j = 0;
+
+    for (int i = 0; i < array.length - 1; i++)
+    {
+      int currentElement = array[i];
+
+      if (currentElement != array[i+1]) {
+        newArray[j++] = currentElement;
+      }
+    }
+
+    newArray[j++] = array[array.length-1];
+
+    return newArray;
   }
+
 
   // Find the length of the longest strictly increasing sequence in the array. Example: in [2, 3, 5, 4] it will be 3, in [5, 4, 3] it will be 1.
   public int findLongestIncreasingSequence(int[] array) {
--- src/main/java/com/github/knifeofdreams/codekata/Runner.java
+++ src/main/java/com/github/knifeofdreams/codekata/Runner.java
@@ -1,31 +1,28 @@
 package com.github.knifeofdreams.codekata;
 
 
-import com.github.knifeofdreams.codekata.HashMapPractice.TrickyObject;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Arrays;
 
 public class Runner {
 
   public static void main(String[] args) {
-    var practice = new HashMapPractice();
-    // This is the minimal compiling code. Set up the HashMap in a way that it's suitable to test your solution.
-    HashMap<String, Integer> map = new HashMap<>();
+    var practice = new ArrayPractice();
+    // This is the minimal compiling code. Construct the array with integers inside to test your solutions.
+    //int[] array = {1, 2, 3, 4, 22, 44, 1, 2};
+    int[] array = {1, 1, 2, 2, 3, 4, 22, 44, 44};
 
-    System.out.println(practice.add(map, "Stockholm", 1));
-    // Add the rest of the tests here. I'll write tests for the last two exercises.
+    //System.out.println(Arrays.toString(practice.add(array, 56)));
+    //System.out.println(practice.get(array, 5));
+    //System.out.println(Arrays.toString(practice.remove(array, 0)));
+    //System.out.println(practice.contains(array, 3));
+    //System.out.println(practice.indexOf(array, 2));
+    //System.out.println(practice.size(array));
+    //System.out.println(Arrays.toString(practice.doubleValues(array)));
+    //System.out.println(Arrays.toString(practice.evenNumbers(array)));
+    System.out.println(Arrays.toString(practice.removeDuplicates(array)));
+    //System.out.println(practice.findLongestIncreasingSequence(array));
+    //System.out.println(practice.sort(array));
 
-    TrickyObject trickyObj1 = new TrickyObject();
-    TrickyObject trickyObj2 = new TrickyObject();
-    TrickyObject trickyObj3 = new TrickyObject();
 
-    List<TrickyObject> trickyObjects = List.of(trickyObj1, trickyObj3, trickyObj2, trickyObj2, trickyObj2, trickyObj1);
-    System.out.println(practice.countInList(trickyObjects));
-
-    // The following result should be true
-    System.out.println(practice.isPangram("thequickbrownfoxjumpsoverthelazydog"));
-
-    // The following result should be false
-    System.out.println(practice.isPangram("JuditIsATroll"));
   }
 }
