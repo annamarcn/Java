36b4a8d25817c61f7a59cf23d9165f9cffd6557e
judito <judito@spotify.com>
2022-03-01 15:10:58 +0000

    Longer add example.

--- src/test/java/leetcode/LinkedListsTest.java
+++ src/test/java/leetcode/LinkedListsTest.java
@@ -7,8 +7,17 @@ class LinkedListsTest {
   private LinkedLists linkedList = new LinkedLists();
 
   @Test
-  public void addingNodeIncreasesListWithAddedNode() {
+  public void canAddOneNodeToEmptyLinkedList() {
     linkedList.add(3);
     assertEquals(linkedList.toString(), "3 -> ");
+  }
+
+  @Test
+  public void canAddMultipleNodesToLinkedList() {
+    linkedList.add(3);
+    linkedList.add(5);
+    linkedList.add(7);
+    linkedList.add(9);
+    assertEquals("3 -> 5 -> 7 -> 9 -> ", linkedList.toString());
   }
 }
